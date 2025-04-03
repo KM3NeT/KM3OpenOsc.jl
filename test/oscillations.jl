a63552815fcadb1908ed42bcf6ca722c8eea5e92
Santiago Pena Martinez <spenamartinez@km3net.de>
2025-04-03 14:59:12 +0200

    Add more unit tests

--- test/oscillations.jl
+++ test/oscillations.jl
@@ -40,24 +40,46 @@ const BINDEF = datapath("oscillations", "bins_433kt-y_v0.4.json")
     hn = create_histograms(BINDEF)
     hd = create_histograms(BINDEF)
     hm = create_histograms(BINDEF)
-    @test true == hasproperty(hn, :hists_true)
-    @test true == hasproperty(hn, :hists_reco)
+    @test hn isa HistogramsOscillations
+    @test 300 == length(hn.hists_reco["recoShowers"].bincounts)
+    @test 3200 == length(hn.hists_true["trueShowers"].bincounts)
+    @test hasproperty(hn, :hists_true)
+    @test hasproperty(hn, :hists_reco)
 
-    flux_dict = get_flux_dict()
-    @test true == haskey(flux_dict, -12)
-    @test true == haskey(flux_dict, 14)
+    NUFLUX_PATH = split(Base.pathof(NuFlux), "src")[1]
+    FLUX_DATA_DIR = joinpath(NUFLUX_PATH, "data")
+    flux_path = joinpath(FLUX_DATA_DIR, "frj-ally-20-12-solmin.d")
+    flux_dict = get_flux_dict(flux_path)
+    @test haskey(flux_dict, -12)
+    @test haskey(flux_dict, 14)
 
-    U,H = get_oscillation_matrices()
+    BF = Dict("dm_21" => 7.42e-5,
+                   "dm_31" => 2.18e-3,
+                   "theta_12" => deg2rad(33.45),
+                   "theta_23" => deg2rad(45.57299599919429),
+                   "theta_13" => deg2rad(8.62),
+                   "dcp" => deg2rad(230))
+
+    U,H = get_oscillation_matrices(BF)
+
     @test true == isa(U, Matrix)
     @test true == isa(H, Vector)
 
-    #fill_response!(hn, nu, flux_dict, U, H; oscillations=true, livetime=1.39)
-    #fill_response!(hd, data)
-    #fill_response!(hm, muons; livetime=1.39)
+    fill_response!(hn, nu, flux_dict, U, H; oscillations=true)
 
-    #@test isapprox(integral(hn.hists_true["true"]), 557.9265650274699)
-    #@test isapprox(integral(hd.hists_reco["recoShowers"]), 235.0)
-    #@test isapprox(integral(hm.hists_reco["recoLowPurityTracks"]), 17.39294089847084)
+    @test isapprox(integral(hd.hists_reco["recoShowers"]), 206.89133463351368)
+
+    hdf5_filename = "test_data.h5"
+    export_histograms_hdf5(hn, hdf5_filename)
+    @test isfile(hdf5_filename)
+    rm(filename)
+
+    fh5 = build_HDF5_file(hdf5_filename)
+    fill_HDF5_file!(fh5, nu, "neutrinos")
+    close(fh5)
+    @test isfile(hdf5_filename)
+    rm(hdf5_filename)
+
 
 
     close(f)
